213328bc9d749b2bcbc8210a3b94055fac9696bf
Dieter Dewachter <dieter.dewachter@student.vives.be>
2021-10-05 16:07:15 +0200

    Used -> instead of .

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -24,32 +24,32 @@ namespace Pulu
         groundTemperature();
     }
 
-    uint16_t Sensor::GetValues()
+    uint16_t * Sensor::GetValues()
     {
         return values;
     }
 
     void Sensor::lightLevel()
     {
-        values[0] = lightSensor.lightLevel();
+        values[0] = lightSensor->lightLevel();
     }
 
     void Sensor::moisture()
     {
-        values[1] = moistureSensorLevel1.moisture();
-        values[2] = moistureSensorLevel2.moisture();
-        values[3] = moistureSensorLevel3.moisture();
-        values[4] = moistureSensorLevel4.moisture();
+        values[1] = moistureSensorLevel1->moisture();
+        values[2] = moistureSensorLevel2->moisture();
+        values[3] = moistureSensorLevel3->moisture();
+        values[4] = moistureSensorLevel4->moisture();
     }
 
     void Sensor::airTemperature()
     {
-        values[5] = airTemperatureSensor.temperature();
+        values[5] = airTemperatureSensor->temperature();
     }
 
     void Sensor::groundTemperature()
     {
-        values[6] = groundTemperatureSensor.temperature();
+        values[6] = groundTemperatureSensor->temperature();
     }
 
     void Sensor::batteryLevel()
