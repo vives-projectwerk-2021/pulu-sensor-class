4c66c2a2d06b3bab5a266875344a86017765cd90
Dimi Catrysse <dimicatrysse@outlook.com>
2021-11-08 16:28:42 +0100

    Changed 8 moisture sensors to 4

--- src/sensorManager.cpp
+++ src/sensorManager.cpp
@@ -9,10 +9,6 @@ namespace Pulu {
             FakeMoistSensor(),
             FakeMoistSensor(),
             FakeMoistSensor(),
-            FakeMoistSensor(),
-            FakeMoistSensor(),
-            FakeMoistSensor(),
-            FakeMoistSensor(),
             FakeMoistSensor()
         };
         temperatureSensors = {
